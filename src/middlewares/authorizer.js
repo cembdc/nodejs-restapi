9b408f023d27352da820dda92e8be55c57cb35de
cembideci <cembdci@gmail.com>
2020-05-24 19:19:38 +0300

    checkAuth method updated

--- src/middlewares/authorizer.js
+++ src/middlewares/authorizer.js
@@ -2,14 +2,24 @@ const jwt = require('jsonwebtoken');
 const { userService } = require('../service/service.index');
 const { config } = require('../config/config');
 
-exports.checkAuth = (req, res, next) => {
+exports.checkAuth = async (req, res, next) => {
 	try {
 		/* JWT is send with request header! 
         Format of it: Authorization : Bearer <token>
         */
 		const token = req.headers.authorization.split(' ')[1];
 		const decodedToken = jwt.verify(token, config.token_config.secret);
-		req.userData = decodedToken;
+
+		const result = await userService.getUser(decodedToken.userId);
+
+		if (!result.success) {
+			return res.status(401).send({
+				success: false,
+				message: 'Authorization failed'
+			});
+		}
+
+		req.userData = result.data;
 		next();
 	} catch (error) {
 		return res.status(401).send({
