40657bd25fcf6f4b1912871acc93771e10e1d7b7
Cem <cembdci@gmail.com>
2020-12-07 02:00:28 +0300

    temp test fix

--- test/utils/setupTestDB.js
+++ test/utils/setupTestDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { config } = require('../config/config');
+const { config } = require('../../src/config/config');
 
 const setupTestDB = () => {
 	before(async () => {
