f6a7e166e263f66140d293400398d52b41a3e3b8
Cem <cembdci@gmail.com>
2020-12-12 21:03:37 +0300

    fix

--- test/utils/setupTestDB.js
+++ test/utils/setupTestDB.js
@@ -5,16 +5,19 @@ const setupTestDB = () => {
 	before(async () => {
 		const conStr = config.db_config.connStr;
 		await mongoose.connect(conStr, { useNewUrlParser: true, autoIndex: false, useUnifiedTopology: true });
+		console.log(`%cMongo connection created: ${config.db_config.connStr}`, 'color: green');
 	});
 
 	beforeEach(async () => {
 		await Promise.all(
 			Object.values(mongoose.connection.collections).map(async collection => collection.deleteMany())
 		);
+		console.log(`%cMongo collections content cleaned`, 'color: green');
 	});
 
 	after(async () => {
 		await mongoose.disconnect();
+		console.log(`%cMongo disconnected`, 'color: green');
 	});
 };
 
