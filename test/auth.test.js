4524a8961ed0c9dbdd9c03614add600629b5d6fb
Cem <cembdci@gmail.com>
2020-12-16 22:40:50 +0300

    auth/login integration test cases completed

--- test/auth.test.js
+++ test/auth.test.js
@@ -134,72 +134,53 @@ describe('Authentication API', () => {
 					expect(data.token).to.not.equal(undefined);
 					done(err);
 				});
-			// .then(res => {
-			// 	expect(res.status).to.equal(httpStatus.OK);
-			// 	expect(res.body).to.have.a.property('success');
-			// 	expect(res.body).to.have.a.property('message');
-			// 	expect(res.body).to.have.a.property('data');
-			// 	expect(res.body.data).to.have.a.property('token');
-			// 	const { message } = res.body;
-			// 	const { success } = res.body;
-			// 	const { data } = res.body;
-			// 	expect(success).to.be.equal(true);
-			// 	expect(message).to.include('Succesfull');
-			// 	expect(data).to.not.equal(null);
-			// 	expect(data).to.not.equal(undefined);
-			// 	expect(data.token).to.not.equal(null);
-			// 	expect(data.token).to.not.equal(undefined);
-			// 	done();
-			// });
-			// .catch(err => {
-			// 	console.log(err);
-			// 	done(err);
-			// });
 		});
 
-		// 	it('should report error when email and password are not provided', () => {
-		// 		return request(app)
-		// 			.post('/v1/auth/login')
-		// 			.send({})
-		// 			.expect(httpStatus.BAD_REQUEST)
-		// 			.then(res => {
-		// 				const { field } = res.body.errors[0];
-		// 				const { location } = res.body.errors[0];
-		// 				const { messages } = res.body.errors[0];
-		// 				expect(field).to.be.equal('email');
-		// 				expect(location).to.be.equal('body');
-		// 				expect(messages).to.include('"email" is required');
-		// 			});
-		// 	});
+		it('should report error when email and password are not provided', done => {
+			request(app)
+				.post('/api/v1/auth/login')
+				.send({})
+				.expect(httpStatus.UNPROCESSABLE_ENTITY)
+				.end((err, res) => {
+					expect(res.body).to.have.a.property('errors');
+					const { errors } = res.body;
+					expect(errors).length.greaterThan(0);
+					expect(errors.some(x => x.message.includes('"email" is required'))).to.be.equal(true);
+					expect(errors.some(x => x.message.includes('"password" is required'))).to.be.equal(true);
+					done(err);
+				});
+		});
 
-		// 	it('should report error when the email provided is not valid', () => {
-		// 		user.email = 'this_is_not_an_email';
-		// 		return request(app)
-		// 			.post('/v1/auth/login')
-		// 			.send(user)
-		// 			.expect(httpStatus.BAD_REQUEST)
-		// 			.then(res => {
-		// 				const { field } = res.body.errors[0];
-		// 				const { location } = res.body.errors[0];
-		// 				const { messages } = res.body.errors[0];
-		// 				expect(field).to.be.equal('email');
-		// 				expect(location).to.be.equal('body');
-		// 				expect(messages).to.include('"email" must be a valid email');
-		// 			});
-		// 	});
+		it('should report error when the email provided is not valid', done => {
+			loginUser.email = 'this_is_not_an_email';
+			request(app)
+				.post('/api/v1/auth/login')
+				.send(loginUser)
+				.expect(httpStatus.UNPROCESSABLE_ENTITY)
+				.end((err, res) => {
+					expect(res.body).to.have.a.property('errors');
+					const { errors } = res.body;
+					expect(errors).length.greaterThan(0);
+					expect(errors[0].message).to.include('"email" must be a valid email');
+					done(err);
+				});
+		});
 
-		// 	it("should report error when email and password don't match", () => {
-		// 		dbUser.password = 'xxx';
-		// 		return request(app)
-		// 			.post('/v1/auth/login')
-		// 			.send(dbUser)
-		// 			.expect(httpStatus.UNAUTHORIZED)
-		// 			.then(res => {
-		// 				const { code } = res.body;
-		// 				const { message } = res.body;
-		// 				expect(code).to.be.equal(401);
-		// 				expect(message).to.be.equal('Incorrect email or password');
-		// 			});
-		// 	});
+		it("should report error when email and password don't match", done => {
+			loginUser.password = '123123wer4';
+			request(app)
+				.post('/api/v1/auth/login')
+				.send(loginUser)
+				.expect(httpStatus.NOT_FOUND)
+				.end((err, res) => {
+					expect(res.body).to.have.a.property('success');
+					expect(res.body).to.have.a.property('message');
+					const { message } = res.body;
+					const { success } = res.body;
+					expect(success).to.be.equal(false);
+					expect(message).to.include('Wrong email or password');
+					done(err);
+				});
+		});
 	});
 });
